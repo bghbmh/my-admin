f0bb7ffbf51229b7a81bff8e9ed2f0c53989f668
miniBig <bghbmh@gmail.com>
2026-03-05 09:56:33 +0900

    수정

--- src/app/(main)/setting/page.tsx
+++ src/app/(main)/setting/page.tsx
@@ -2,26 +2,10 @@
 //export const dynamic = 'force-dynamic';
 //export const revalidate = 0;
 
-import { MAIN_CATEGORY } from "@/constants/config";
-import { ProjectDataType } from "@/types/project.data";
-
-import fs from "fs/promises";
-import path from "path";
-
 import PageHeader from "@/components/common/page-header";
 import SettingsClient from "./SettingsClient";
 
-let testList = 0;
-
-
-import { DB_PATH } from "@/constants/paths";
-
 export default async function ArchivePage() {
-
-	// 1. 서버에서 직접 최신 JSON 파일 읽기 
-	const fileContent = await fs.readFile(DB_PATH, "utf-8");
-	const projectList: ProjectDataType[] = JSON.parse(fileContent);
-
 
 	return (
 		<>
